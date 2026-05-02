b729f0773ce5202a8fa72f61cddfdab3a677adb5
AzizBenIsmail <aziz.270700@gmail.com>
2026-05-02 12:26:46 +0100

    s1

---
+++ src/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function NavBar() {
+    // logic
+    const age = 20 + 4
+  return (
+    <div>
+      hello NavBar
+         age : {age}
+      {/* vue */}
+    </div>
+  )
+}
