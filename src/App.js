5d88068231061437c391db50749a2e2b279cd8cd
AzizBenIsmail <aziz.270700@gmail.com>
2026-05-09 10:25:47 +0100

    s2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './NavBar';
-import Footer from './footer';
+import "./App.css";
+
 function App() {
+  //logic
+  //1/
+  const name = "aziz";
+  const age = 20;
+  const isLoggedIn = false;
+  const element = <div>hello {name}</div>;
+
+  //2/
+  const etudiant = {
+    nom: "mohamed",
+    prenom: "ben ismail",
+  };
+  //3/
+  function getData(data) {
+    return "hello " + data + " fi 9antra";
+  }
+
+  //4
+  const listEtudiant = [
+    { nom: "mohamed", prenom: "ben foulen", age: 20, ObjectId: 1 },
+    { nom: "ines", prenom: "ben foulen", age: 22, ObjectId: 2 },
+    { nom: "sami", prenom: "ben foulen", age: 21, ObjectId: 3 },
+  ];
+
+  const style = {
+    color: "red",
+    fontSize: "20px",
+  };
   return (
     <div className="App">
-      <NavBar />
+      {/* jsx => html + js */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        1/
+        <br />
+        {name}
+        <br />
+        {age}
+        {isLoggedIn && <p>You are logged in!</p>}
+        {element}
+        2/
+        <br />
+        {etudiant.nom} {etudiant.prenom}
+        3/
+        <br />
+        {getData("ines")}
+        4/
+        <br />
+        {/* <ul style={{ color: "pink", fontSize: "20px" }}> */}
+        {/* <ul style={style}> */}
+        <ul className="direction">
+            {listEtudiant.map((etudiant) => (
+            <li key={etudiant.ObjectId}>
+              {etudiant.nom} {etudiant.prenom} {etudiant.age}
+            </li>
+          ))}
+        </ul>
+        {/* vue */}
+        <table border={1}>
+          <tr>
+            <th>Nom</th>
+            <th>Prenom</th>
+            <th>Age</th>
+          </tr>
+          {listEtudiant.map((etudiant) => (
+            <tr key={etudiant.ObjectId}>
+              <td>{etudiant.nom}</td>
+              <td>{etudiant.prenom}</td>
+              <td>{etudiant.age}</td>
+            </tr>
+          ))}
+        </table>
       </header>
-      <Footer />
     </div>
   );
 }
